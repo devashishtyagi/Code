dbcf6c1c61ee7f42ca683ddcdf19c29c375c3dbc
Devashish Tyagi <devashishrocker@gmail.com>
2013-06-10 04:20:25 +0530

    Round 187

--- Codeforces/187/c.cpp
+++ Codeforces/187/c.cpp
@@ -37,13 +37,55 @@
 #define init0(a) memset((a),0,sizeof(a))
 
 #define INF 1000000000
-#define MOD 1000000009
+#define MOD 1000000007
 #define PI 3.1415926535897932
 
 using namespace std;
 typedef long long LL;
 
+const int MAXN = 1000002;
+int tree[MAXN];
+int q[MAXN];
+
+void add(int index, int v) {
+	while(index <= MAXN) {
+		tree[index] = ((LL)tree[index] + v)%MOD;
+		index += (index & -index);
+	}
+}
+
+int get(int index) {
+	int sum = 0;
+	while(index > 0) {
+		sum = ((LL)tree[index]+sum)%MOD;
+		index -= (index & -index);
+	}
+	return sum;
+}
+
 int main()
 {
+	int n;
+	cin>>n;
 
+	init0(tree);
+	init0(q);
+
+	int a[n];
+
+	forn(i, 0, n)
+		cin>>a[i];
+
+	forn(i, 0, n) {
+		int new_val = ((LL)get(a[i])*1LL*a[i]%MOD + a[i])%MOD;
+		int change = ((LL)new_val - q[a[i]])%MOD;
+		if (change < 0)
+			change += MOD;
+		q[a[i]] = new_val;
+		add(a[i], change);
+	}
+
+	cout<<get(MAXN-1)<<endl;
+
+	return 0;
 }
