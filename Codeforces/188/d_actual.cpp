11d490de736421280b7e9b55ee75541fae968253
monalisag <monalisa.vmc@gmail.com>
2013-06-25 15:54:09 +0530

    Problem name corrected for round 188

---
+++ Codeforces/188/d_actual.cpp
@@ -0,0 +1,130 @@
+/* CPP Tempelate
+ * @author Devashish Tyagi
+ */
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <fstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <list>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#define sf(a) scanf("%d",&a)
+#define ssf(a,b) scanf("%d %d",&a,&b)
+#define pf(a) printf("%d\n",a)
+#define foreach(container, it) for(typeof(container.begin()) it = container.begin(); it != container.end(); it++)
+#define pi pair<int,int>
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+
+#define pb push_back
+#define mp make_pair
+#define sz(a) (int)(a).size()
+
+#define forn(i,a,b) for(int (i) = (a); (i) < (b); ++(i))  
+#define rforn(i,a,b) for(int (i) = (a)-1; (i) >= (b); --(i))  
+#define init0(a) memset((a),0,sizeof(a))
+
+#define INF 1000000000
+#define MOD 1000000009
+#define PI 3.1415926535897932
+
+using namespace std;
+typedef long long LL;
+
+int grundy[] = {
+	1,
+	2,
+	1,
+	4,
+	3,
+	2,
+	1,
+	5,
+	6,
+	2,
+	1,
+	8,
+	7,
+	5,
+	9,
+	8,
+	7,
+	3,
+	4,
+	7,
+	4,
+	2,
+	1,
+	10,
+	9,
+	3,
+	6,
+	11,
+	12
+};
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int limit = (int)sqrt(n);
+
+    int count[30];
+    bool marked[limit+1];
+    init0(count);
+    init0(marked);
+
+    int total_count = 1;
+    count[1] = 1;
+    marked[1] = true;
+
+    for(int i = 2; i <= limit; i++) {
+        if (!marked[i]) {
+            int x = 1;
+            int element = i;
+            marked[element] = true;
+
+            while((element*1LL*i) <= n ) {
+                x++;
+                if (element*i <= limit)
+                	marked[element*i] = true;
+                element *= i;
+            }
+
+            count[x]++;
+            total_count += x;
+        }
+    }
+
+
+    count[1] += (n - total_count);
+
+    int xor_sum = 0;
+
+    for(int i = 1; i <= 29; i++) { 
+        if (count[i]%2 == 1)
+            xor_sum ^= grundy[i-1];
+    }
+
+    if (xor_sum ==  0) {
+        cout<<"Petya"<<endl;
+    }
+    else {
+        cout<<"Vasya"<<endl;
+    }
+
+    return 0;
+}
